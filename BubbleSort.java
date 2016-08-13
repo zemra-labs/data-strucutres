fbadf0bfb465b2e030fc5c813fb04a042f1e0d8c
andimitre <23andimitre@gmail.com>
2016-08-12 21:38:15 -0400

    bubblesort java

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class BubbleSort {
+  public static int temp;
+  static int[] myArray = {10, 50, 40, 30, 20, 100, 80, 70, 90};
+
+  public static void main (String []args) {
+      BubbleSort s = new BubbleSort();
+      s.sort(myArray);
+
+  }
+
+  public static void sort(int[] myArray){
+    for(int i = myArray.length-1; i>1; i--){
+      for (int j = 0; j < i; j++){
+        if(myArray[j] > myArray[j+1]){
+          temp = myArray[j];
+          myArray[j] = myArray[j+1];
+          myArray[j+1] = temp;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(myArray));
+  }
+}
