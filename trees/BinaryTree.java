a06c368c28c335fcbf8b64c32af066432021d4e3
andimitre <23andimitre@gmail.com>
2016-09-01 15:31:35 -0400

    added tree traversals

--- trees/BinaryTree.java
+++ trees/BinaryTree.java
@@ -31,6 +31,34 @@ public class BinaryTree {
     }
   }
 
+  // big O complexity: search, insert, delete O (log(n))
+
+  // preOrder traversal, root, left subtree, right subtree
+  public void preOrderTraverse(Node root){
+    if(root != null) {
+      System.out.println(root.value);
+      preOrderTraverse(root.leftChild);
+      preOrderTraverse(root.righChild);
+    }
+  }
+  // postOrder traversal, left subtree, right subtree, root
+  public void postOrderTraverse(Node root){
+    if(root != null) {
+      postOrderTraverse(root.leftChild);
+      postOrderTraverse(root.rightChild);
+      System.out.println(root.value);
+    }
+  }
+  // inorder traversal, left subtree, root, right subtree
+  public void inOrderTraversal(Node root){
+    if (root != null) {
+      inOrderTraversal(root.leftChild);
+      System.out.println(root.value);
+      inOrderTraversal(root.rightChild);
+    }
+  }
+
+
   public static void main(String []args){
     BinaryTree bt = new BinaryTree();
 
@@ -41,6 +69,8 @@ public class BinaryTree {
     bt.addNode(72);
     bt.addNode(50);
     bt.addNode(2);
+
+
   }
 }
 
